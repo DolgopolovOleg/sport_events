e0d82749999e18ad939641d206e3f12a53774c45
DolgopolovOleg <allegamex@gmail.com>
2014-12-19 00:24:22 +0300

    Add up to date sql dump

--- db/sport_events.sql
+++ db/sport_events.sql
@@ -30,11 +30,11 @@ CREATE TABLE `comment` (
   PRIMARY KEY (`_id`),
   KEY `Users_id` (`user_id`),
   CONSTRAINT `Users_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `comment` */
 
-insert  into `comment`(`_id`,`user_id`,`text`,`created`,`from`,`from_id`) values (1,1,'First comment to place 1','2014-12-07 22:06:15','PLACE',1),(2,2,'Second comment to place 1','2014-12-07 22:06:46','PLACE',1),(3,1,'Comment to event 1','2014-12-07 22:07:38','EVENT',1),(4,2,'Comment to event 1','2014-12-07 22:08:29','EVENT',1);
+insert  into `comment`(`_id`,`user_id`,`text`,`created`,`from`,`from_id`) values (1,1,'sdfgsdfg','2014-12-14 23:46:36','PLACE',4),(2,2,'sdfgsdfg','2014-12-13 23:46:33','PLACE',4);
 
 /*Table structure for table `equipment` */
 
@@ -72,7 +72,7 @@ CREATE TABLE `event` (
 
 /*Data for the table `event` */
 
-insert  into `event`(`_id`,`name`,`sport`,`description`,`place_id`,`date_start`,`date_finish`) values (1,'First',1,'Some description',1,'2014-11-30 20:29:33','2014-12-02 20:29:38');
+insert  into `event`(`_id`,`name`,`sport`,`description`,`place_id`,`date_start`,`date_finish`) values (1,'first',1,'Some description to event',4,'2014-12-16 22:13:27','2014-12-16 22:13:29');
 
 /*Table structure for table `participant` */
 
@@ -82,17 +82,17 @@ CREATE TABLE `participant` (
   `_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
-  `role` varchar(255) DEFAULT NULL,
+  `role` enum('ORGANIZER','PARTICIPANT') DEFAULT NULL,
   PRIMARY KEY (`_id`),
   KEY `Eventid_rel` (`event_id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `event_id` FOREIGN KEY (`event_id`) REFERENCES `event` (`_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `participant` */
 
-insert  into `participant`(`_id`,`user_id`,`event_id`,`role`) values (1,1,1,'organizer'),(2,2,1,'participant');
+insert  into `participant`(`_id`,`user_id`,`event_id`,`role`) values (2,1,1,'ORGANIZER'),(3,2,1,'PARTICIPANT');
 
 /*Table structure for table `place` */
 
@@ -108,11 +108,11 @@ CREATE TABLE `place` (
   PRIMARY KEY (`_id`),
   KEY `creator_id` (`creator`),
   CONSTRAINT `creator_id` FOREIGN KEY (`creator`) REFERENCES `user` (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
 /*Data for the table `place` */
 
-insert  into `place`(`_id`,`name`,`longitude`,`latitude`,`description`,`creator`) values (1,'Первое поле',111,111,'Описание первого поля',1),(2,'Второе поле',222,222,'Описание второго поля',2),(3,'Третье поле',333,333,'Описание третьего поля',1);
+insert  into `place`(`_id`,`name`,`longitude`,`latitude`,`description`,`creator`) values (4,'first',111,111,'Some description to place',2),(5,'second',222,222,'asdasdasd',1);
 
 /*Table structure for table `place_equipment` */
 
@@ -128,11 +128,25 @@ CREATE TABLE `place_equipment` (
   KEY `place_id` (`pl_id`),
   CONSTRAINT `equipment_id` FOREIGN KEY (`eq_id`) REFERENCES `equipment` (`_id`),
   CONSTRAINT `place_id` FOREIGN KEY (`pl_id`) REFERENCES `place` (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 /*Data for the table `place_equipment` */
 
-insert  into `place_equipment`(`_id`,`pl_id`,`eq_id`,`count`) values (1,1,1,2),(2,1,2,2),(3,1,3,1),(4,2,4,1),(5,3,5,2),(8,1,5,2);
+insert  into `place_equipment`(`_id`,`pl_id`,`eq_id`,`count`) values (1,4,1,2),(2,4,2,1),(3,4,3,1);
+
+/*Table structure for table `role` */
+
+DROP TABLE IF EXISTS `role`;
+
+CREATE TABLE `role` (
+  `_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` enum('ADMIN','USER') DEFAULT NULL,
+  PRIMARY KEY (`_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+/*Data for the table `role` */
+
+insert  into `role`(`_id`,`title`) values (1,'ADMIN'),(2,'USER');
 
 /*Table structure for table `sport` */
 
@@ -160,30 +174,29 @@ CREATE TABLE `user` (
   `nickname` char(255) DEFAULT NULL,
   `phone` char(31) DEFAULT NULL,
   `email` char(255) DEFAULT NULL,
+  `username` char(255) DEFAULT NULL,
+  `password` char(255) DEFAULT NULL,
+  `enabled` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 /*Data for the table `user` */
 
-insert  into `user`(`_id`,`name`,`sname`,`nickname`,`phone`,`email`) values (1,'Changed','user','nick','123123','email1@email.com'),(2,'second','user','nick','321321','email2@email.com'),(4,'third','asdf','asd','123 123','asdasdasd@asd.as'),(9,'SВАП','ЫВАП','SDFG','123123','asd@asd.as'),(10,'Имя','asdf','asdf','+73453434345','allegamex@gmail.com');
+insert  into `user`(`_id`,`name`,`sname`,`nickname`,`phone`,`email`,`username`,`password`,`enabled`) values (1,'Oleg','Dolgopolov','Allega','123123','allegameex@yandex.ru','oleg','202cb962ac59075b964b07152d234b70',1),(2,'Second','Second sname','snickname','123123','email','second','202cb962ac59075b964b07152d234b70',1);
 
-/*Table structure for table `users_auth` */
+/*Table structure for table `user_role` */
 
-DROP TABLE IF EXISTS `users_auth`;
+DROP TABLE IF EXISTS `user_role`;
 
-CREATE TABLE `users_auth` (
+CREATE TABLE `user_role` (
   `user_id` int(11) NOT NULL,
-  `login` char(255) NOT NULL,
-  `password` char(255) NOT NULL,
-  `enabled` tinyint(1) DEFAULT '0',
-  `role` varchar(15) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  CONSTRAINT `User_id_rel` FOREIGN KEY (`user_id`) REFERENCES `user` (`_id`)
+  `role_id` int(11) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-/*Data for the table `users_auth` */
+/*Data for the table `user_role` */
 
-insert  into `users_auth`(`user_id`,`login`,`password`,`enabled`,`role`) values (1,'user1','user1',1,NULL),(2,'user2','user2',0,NULL);
+insert  into `user_role`(`user_id`,`role_id`) values (1,1),(1,2),(2,1);
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
